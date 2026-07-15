e7b16cdad2532b80c8f44e89a6552eee976e20a0
Ajetsri <sri291399@gmail.com>
2026-07-15 22:10:26 +0530

    update

--- models/Task.js
+++ models/Task.js
@@ -5,6 +5,14 @@ const taskSchema = new mongoose.Schema({
     task: {
         type: String,
         required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    completed: {
+        type: Boolean,
+        default:false
     }
 
 });
